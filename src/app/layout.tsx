a57428b042e12544923017a660d805c9e4a5276e
CristopherMartarello <crisrossi1313@gmail.com>
2026-03-10 23:40:32 -0300

    feat: add Sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Inter } from 'next/font/google';
+import Sidebar from '@/components/sidebar/sidebar';
 
 export const metadata: Metadata = {
   title: 'Prompt Manager',
@@ -20,9 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn('font-sans', 'font-sans', inter.variable)}>
+    <html lang="en" className={cn('font-sans', inter.variable)}>
       <body className={`${inter.variable} antialiased bg-gray-900 text-white`}>
         <section className="flex h-screen">
+          <Sidebar />
           <main className="relative flex-1 overflow-auto min-w-0">
             <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
               {children}
---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+import SidebarContent from './sidebar-content';
+
+const Sidebar = () => {
+  return <SidebarContent />;
+};
+
+export default Sidebar;
