33462d85741dbc660577646d6fcee6e2d5d55e81
CristopherMartarello <crisrossi1313@gmail.com>
2026-03-11 22:28:27 -0300

    test: add tests to SidebarContent and its behavarios

---
+++ src/tests/components/sidebar/sidebar-content.spec.tsx
@@ -0,0 +1,60 @@
+import SidebarContent from '@/components/sidebar/sidebar-content';
+import { render, screen } from '@/lib/test-utils';
+import userEvent from '@testing-library/user-event';
+
+// mockando o useRouter do next para realizarmos o teste
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({ push: jest.fn() }),
+}));
+
+// quem estamos testando nesse teste? Sut = System under test
+const makeSut = () => {
+  return render(<SidebarContent />);
+};
+
+describe('SidebarContent', () => {
+  const user = userEvent.setup();
+
+  it('should render a new prompt button', () => {
+    makeSut();
+
+    expect(screen.getByRole('complementary')).toBeVisible();
+    expect(screen.getByRole('button', { name: 'Novo prompt' })).toBeVisible();
+  });
+
+  describe('Colapsar / Expandir', () => {
+    it('should start expanded and display minimize button', () => {
+      makeSut();
+
+      const aside = screen.getByRole('complementary');
+      expect(aside).toBeVisible();
+
+      const collapseButton = screen.getByRole('button', {
+        name: /minimizar sidebar/i, //i -> insensitivo, sem case sensitive
+      });
+      expect(collapseButton).toBeVisible();
+
+      // queryByRole -> retorna null se não há um nó correspondente
+      const expandButton = screen.queryByRole('button', {
+        name: /expandir sidebar/i,
+      });
+      expect(expandButton).not.toBeInTheDocument();
+    });
+
+    it('should collapse and show expand button', async () => {
+      makeSut();
+
+      const collapseButton = screen.getByRole('button', {
+        name: /minimizar sidebar/i, //i -> insensitivo, sem case sensitive
+      });
+
+      await user.click(collapseButton);
+
+      const expandButton = screen.queryByRole('button', {
+        name: /expandir sidebar/i,
+      });
+      expect(expandButton).toBeInTheDocument();
+      expect(collapseButton).not.toBeInTheDocument();
+    });
+  });
+});
