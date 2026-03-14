e5e94a73639a420f924a75eca6655286173adce8
CristopherMartarello <crisrossi1313@gmail.com>
2026-03-14 18:26:26 -0300

    feat: migrate prisma to v7 and creating db seed to prompts with fakerjs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,11 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-const prismaClient = new PrismaClient();
-export { prismaClient as prisma };
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
