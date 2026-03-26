ce63a36a8a245e99fb0061ae45cdbfd39f720117
CristopherMartarello <crisrossi1313@gmail.com>
2026-03-26 20:19:52 -0300

    feat: add prompt-form input error test

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,9 +1,22 @@
 import { prisma } from '@/lib/prisma';
 import SidebarContent from './sidebar-content';
+import { PrismaPromptRepository } from '@/infra/repository/prisma-prompt.repository';
+import { PromptSummary } from '@/core/domain/prompts/prompt.entity';
 
 const Sidebar = async () => {
-  const prompts = await prisma.prompt.findMany();
-  return <SidebarContent prompts={prompts} />;
+  const repository = new PrismaPromptRepository(prisma);
+  let initialPrompts: PromptSummary[] = [];
+
+  try {
+    const prompts = await repository.findMany();
+    initialPrompts = prompts.map((prompt) => ({
+      ...prompt,
+    }));
+  } catch {
+    initialPrompts = [];
+  }
+
+  return <SidebarContent prompts={initialPrompts} />;
 };
 
 export default Sidebar;
