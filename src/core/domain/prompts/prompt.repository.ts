2d32b7d7a5dab83ec0dba04bea617ed010dcfdd7
CristopherMartarello <crisrossi1313@gmail.com>
2026-03-28 14:52:57 -0300

    feat: TDD example to prompt-update action

--- src/core/domain/prompts/prompt.repository.ts
+++ src/core/domain/prompts/prompt.repository.ts
@@ -4,6 +4,7 @@ import { Prompt } from './prompt.entity';
 export interface PromptRepository {
   create(data: createPromptDTO): Promise<void>;
   findMany(): Promise<Prompt[]>;
+  findById(id: string): Promise<Prompt | null>;
   findByTitle(title: string): Promise<Prompt | null>;
   searchMany(term: string): Promise<Prompt[]>;
 }
